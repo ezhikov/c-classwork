754e203767b85c7fd525b5a7383c091e16a8c285
unknown <fon.igel@gmail.com>
2015-05-03 16:20:01 +0300

    sum from 1 to n

---
+++ for.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int i, sum = 0, n;
+  /*for(i=0; i < 10; i++) {
+    if(i==5) return 0;
+    printf("%d\n", i);    
+  }*/
+  
+  /* Вычислить сумму чисел от 1 до n */
+  scanf("%d", &n);
+  for(i = 1; i <= n; i++) sum +=i;
+  printf("%d\n", sum);
+  
+  system("PAUSE");	
+  return 0;
+}
